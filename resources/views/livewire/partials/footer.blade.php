1857d4c9ca5abd7d8b5d1f9f68e44764e7796cd8
jamtercenio23 <jamtercenio23@gmai.com>
2024-05-14 21:38:54 +0800

    Make initial user panel

---
+++ resources/views/livewire/partials/footer.blade.php
@@ -0,0 +1,42 @@
+<footer class="bg-gray-900 w-full">
+    <div class="w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 lg:pt-20 mx-auto">
+      <!-- Grid -->
+      <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6">
+        <div class="col-span-full lg:col-span-1">
+          <a class="flex-none text-xl font-semibold text-white dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#" aria-label="Brand">StyleLab</a>
+        </div>
+        <!-- End Col -->
+
+        <div class="col-span-1">
+          <h4 class="font-semibold text-gray-100">Product</h4>
+
+          <div class="mt-3 grid space-y-3">
+            <p><a class="inline-flex gap-x-2 text-gray-400 hover:text-gray-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="/categories">Categories</a></p>
+            <p><a class="inline-flex gap-x-2 text-gray-400 hover:text-gray-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="/products">All Products</a></p>
+            <p><a class="inline-flex gap-x-2 text-gray-400 hover:text-gray-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="/products">Featured Products</a></p>
+          </div>
+        </div>
+        <!-- End Col -->
+
+        <div class="col-span-1">
+          <h4 class="font-semibold text-gray-100">Company</h4>
+
+          <div class="mt-3 grid space-y-3">
+            <p><a class="inline-flex gap-x-2 text-gray-400 hover:text-gray-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">About us</a></p>
+            <p><a class="inline-flex gap-x-2 text-gray-400 hover:text-gray-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">Blog</a></p>
+
+            <p><a class="inline-flex gap-x-2 text-gray-400 hover:text-gray-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">Customers</a></p>
+          </div>
+        </div>
+
+      </div>
+      <!-- End Grid -->
+
+      <div class="mt-5 sm:mt-12 grid gap-y-2 sm:gap-y-0 sm:flex sm:justify-between sm:items-center">
+        <div class="flex justify-between items-center">
+          <p class="text-sm text-gray-400">© 2024 Style Lab. All rights reserved.</p>
+        </div>
+        <!-- End Col -->
+      </div>
+    </div>
+  </footer>
